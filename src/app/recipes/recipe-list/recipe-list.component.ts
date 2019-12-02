4be782ffe03e747e33959f6f63e743632d630dda
Jeev Arumugam <jeevaweb17@gmail.com>
2019-12-02 15:41:56 -0500

    recipe component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Recipe } from "../recipe.model";
 
 @Component({
   selector: "app-recipe-list",
@@ -6,7 +7,13 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./recipe-list.component.css"]
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      "A Test Recipe",
+      "This is a test description",
+      "https://pixabay.com/get/52e1d24a4356b10ff3d89938b977692b083edbe25652704e742679/shish-kebab-417994_640.jpg"
+    )
+  ];
   constructor() {}
 
   ngOnInit() {}
